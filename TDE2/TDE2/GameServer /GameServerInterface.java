931d5bee2417c493be262b030c37fb2cddb5fedb
HMarchiori <hmarchiori2003@gmail.com>
2024-06-05 22:35:02 -0300

    wtf tem um erro

---
+++ TDE2/TDE2/GameServer /GameServerInterface.java
@@ -0,0 +1,9 @@
+import TDE2.T1.*;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface GameServerInterface extends Remote{
+    void registerClient(String clientId) throws RemoteException;
+    void sendCommand(String clientId, int sequenceNumber, int posX, int posY, int numMoedas) throws RemoteException;
+    GameState getGameState() throws RemoteException;
+}
--- TDE2/TDE2/GameServer /GameState.java
+++ TDE2/TDE2/GameServer /GameState.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class GameState implements Serializable {
-    private static final long serialVersionUID = 1L; // Identificador único para a classe
 
     private Mapa mapa;
     private int contadorMoedas;
@@ -20,7 +19,6 @@ public class GameState implements Serializable {
         posY = mapa.getY();
     }
 
-    // Getters e setters
     public Mapa getMapa() {
         return mapa;
     }
@@ -61,13 +59,11 @@ public class GameState implements Serializable {
         this.posY = posY;
     }
 
-    // Métodos para coletar moedas
     public void coletarMoeda(Moeda moeda) {
         moedasColetadas.add(moeda);
         contadorMoedas++;
     }
 
-    // Métodos para atualizar a posição do jogador
     public void atualizarPosicao(int novoX, int novoY) {
         this.posX = novoX;
         this.posY = novoY;
