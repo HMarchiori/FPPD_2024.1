dddf78cf9115df252e393e49b35ad6a1dd01f625
HMarchiori <hmarchiori2003@gmail.com>
2024-06-10 17:54:06 -0300

    FINAL

--- TDE2/GameClient.java
+++ TDE2/GameClient.java
@@ -8,15 +8,12 @@ public class GameClient {
     private GameServerInterface gameServer;
     private GameState gameState;
     private String clientId;
-    @SuppressWarnings("unused")
-    private int sequenceNumber;
 
     public GameClient(String serverAddress) {
         try {
             Registry registry = java.rmi.registry.LocateRegistry.getRegistry(serverAddress);
             gameServer = (GameServerInterface) registry.lookup("GameServer");
             this.clientId = java.util.UUID.randomUUID().toString();
-            this.sequenceNumber = 0;
             gameServer.registerClient(clientId);
             new Thread(this::fetchUpdates).start();
         } catch (Exception e) {
@@ -40,16 +37,6 @@ public class GameClient {
         }
     }
 
-    /* private void sendCommand(int posX, int posY, int coinCounter) {
-        try {
-            sequenceNumber++;
-            gameServer.sendCommand(clientId, sequenceNumber, posX, posY, coinCounter);
-        } catch (Exception e) {
-            System.err.println("GameClient exception: " + e.toString());
-            e.printStackTrace();
-        }
-    }
-        */
 
     public static void main(String[] args) {
         if (args.length != 1) {
